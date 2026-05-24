a66e3ec500bf55a9dee96dbc67f657065346a630
Hexon <snicks.phyla0y@icloud.com>
2026-05-23 22:09:51 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // firebase-config.js
 export const firebaseConfig = {
   apiKey: "AIzaSyCceZk8s3Xk0QH9rZ8jrRCL9kAoy2Nsr48",
-  authDomain: "auth.hijake.xyz",
+  authDomain: "codequestbyjake.firebaseapp.com",
   projectId: "codequestbyjake",
   appId: "1:391393248347:web:9cc84920abe82369c82b7e",
 };
